7944df577817663d4897697a30d475b5d8cdd92e
Dheeraj Bisht <deekosalam@gmail.com>
2024-01-24 23:42:03 +0530

    Added Initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "App name",
+    template: "%s | App name",
+  },
+  description: "App description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} min-w-[350px]`}>{children}</body>
+    </html>
+  );
+}
